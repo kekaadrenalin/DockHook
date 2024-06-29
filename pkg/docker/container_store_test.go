54d2a3cc027017a4107fe957ef1ab9677f870692
kekaadrenalin <kekapor@outlook.com>
2024-06-29 14:10:55 +0300

    Fix tests

--- pkg/docker/container_store_test.go
+++ pkg/docker/container_store_test.go
@@ -5,57 +5,56 @@ import (
 
 	"testing"
 
-	myTypes "github.com/kekaadrenalin/dockhook/pkg/types"
-
+	"github.com/kekaadrenalin/dockhook/pkg/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
 type mockedClient struct {
 	mock.Mock
-	myTypes.Client
+	types.Client
 }
 
-func (m *mockedClient) ListContainers() ([]myTypes.Container, error) {
+func (m *mockedClient) ListContainers() ([]types.Container, error) {
 	args := m.Called()
-	return args.Get(0).([]myTypes.Container), args.Error(1)
+	return args.Get(0).([]types.Container), args.Error(1)
 }
 
-func (m *mockedClient) FindContainerByID(id string) (myTypes.Container, error) {
+func (m *mockedClient) FindContainerByID(id string) (types.Container, error) {
 	args := m.Called(id)
-	return args.Get(0).(myTypes.Container), args.Error(1)
+	return args.Get(0).(types.Container), args.Error(1)
 }
 
-func (m *mockedClient) Events(ctx context.Context, events chan<- myTypes.ContainerEvent) error {
+func (m *mockedClient) Events(ctx context.Context, events chan<- types.ContainerEvent) error {
 	args := m.Called(ctx, events)
 	return args.Error(0)
 }
 
-func (m *mockedClient) Host() *myTypes.Host {
+func (m *mockedClient) Host() *types.Host {
 	args := m.Called()
-	return args.Get(0).(*myTypes.Host)
+	return args.Get(0).(*types.Host)
 }
 
 func TestContainerStore_List(t *testing.T) {
 
 	client := new(mockedClient)
-	client.On("ListContainers").Return([]myTypes.Container{
+	client.On("ListContainers").Return([]types.Container{
 		{
 			ID:   "1234",
 			Name: "test",
 		},
 	}, nil)
-	client.On("Events", mock.Anything, mock.AnythingOfType("chan<- docker.ContainerEvent")).Return(nil).Run(func(args mock.Arguments) {
+	client.On("Events", mock.Anything, mock.AnythingOfType("chan<- types.ContainerEvent")).Return(nil).Run(func(args mock.Arguments) {
 		ctx := args.Get(0).(context.Context)
 		<-ctx.Done()
 	})
-	client.On("Host").Return(&myTypes.Host{
+	client.On("Host").Return(&types.Host{
 		ID: "localhost",
 	})
 	ctx, cancel := context.WithCancel(context.Background())
 	t.Cleanup(cancel)
 
-	store := myTypes.NewContainerStore(ctx, client)
+	store := types.NewContainerStore(ctx, client)
 	containers, _ := store.List()
 
 	assert.Equal(t, containers[0].ID, "1234")
@@ -63,7 +62,7 @@ func TestContainerStore_List(t *testing.T) {
 
 func TestContainerStore_die(t *testing.T) {
 	client := new(mockedClient)
-	client.On("ListContainers").Return([]myTypes.Container{
+	client.On("ListContainers").Return([]types.Container{
 		{
 			ID:    "1234",
 			Name:  "test",
@@ -74,24 +73,24 @@ func TestContainerStore_die(t *testing.T) {
 	client.On("Events", mock.Anything, mock.AnythingOfType("chan<- types.ContainerEvent")).Return(nil).
 		Run(func(args mock.Arguments) {
 			ctx := args.Get(0).(context.Context)
-			events := args.Get(1).(chan<- myTypes.ContainerEvent)
-			events <- myTypes.ContainerEvent{
+			events := args.Get(1).(chan<- types.ContainerEvent)
+			events <- types.ContainerEvent{
 				Name:    "die",
 				ActorID: "1234",
 				Host:    "localhost",
 			}
 			<-ctx.Done()
 		})
-	client.On("Host").Return(&myTypes.Host{
+	client.On("Host").Return(&types.Host{
 		ID: "localhost",
 	})
 
 	ctx, cancel := context.WithCancel(context.Background())
 	t.Cleanup(cancel)
-	store := myTypes.NewContainerStore(ctx, client)
+	store := types.NewContainerStore(ctx, client)
 
 	// Wait until we get the event
-	events := make(chan myTypes.ContainerEvent)
+	events := make(chan types.ContainerEvent)
 	store.Subscribe(ctx, events)
 	<-events
 
